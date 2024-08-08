fddb9e90b4d7d92b6cec477bc86d46b784d1797b
RidanWeb <ridankabir0wr@gmail.com>
2024-08-08 19:23:07 +0600

    DOM 90% First Chapter

--- script.js
+++ script.js
@@ -13,3 +13,38 @@ const getASection = document.getElementById('countrySec');
 getASection.classList.remove('font-lg');
 getASection.classList.add('text-center');
 
+
+
+//==================JavaScript HTML DOM Navigation========================
+
+//option 01
+//document.getElementById('pContent').innerHTML = document.getElementById('h6Content').innerHTML;
+
+//option 02
+//document.getElementById('pContent').innerHTML = document.getElementById('h6Content').firstChild.nodeValue;
+
+//option 03
+document.getElementById('pContent').innerHTML = document.getElementById('h6Content').childNodes[0].nodeValue;
+
+//============Node Name Value and Type=================
+
+document.getElementById('nN').innerHTML = document.getElementById('NVT').nodeName;
+document.getElementById('nT').innerHTML = document.getElementById('NVT').nodeType;
+
+
+//===============================DOM Node========================================
+
+const newLi = document.createElement('li');
+
+//type 01===============================
+newLi.innerHTML = 'New li in List';
+
+const placeList = document.getElementById('DomNode');
+placeList.appendChild(newLi)
+//type 01===============================
+
+//type 02===============================
+const textNode = document.createTextNode('Another li in List');
+newLi.appendChild(textNode);
+placeList.appendChild(newLi)
+//type 02===============================
